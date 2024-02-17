90ce25c9954b19b99371edc68e19c1ddea212d2f
Manish Kumar <mk1849253@gmail.com>
2024-02-17 16:32:40 +0530

    minor changes in invite modal

--- discord-clone/components/providers/modal-provider.tsx
+++ discord-clone/components/providers/modal-provider.tsx
@@ -2,22 +2,21 @@
 
 import { useEffect, useState } from "react";
 
+import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
   if (!isMounted) {
     return null;
   }
-
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
-  )
-}
+  );
+};
